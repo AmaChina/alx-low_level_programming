1a50b5c50bed029f341eeae4960a5e59d07e169a
AmaChina <nanaama91@hotmail.com>
2023-05-03 11:58:15 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - prints string in reverse
+ * @s: string to be printed
+ * return:
+ */
+
+void print_rev(char *s)
+{
+	int n = 0;
+
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	for (n -= 1; n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}
