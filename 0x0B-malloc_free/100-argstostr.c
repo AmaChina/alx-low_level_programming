2cacf2eba600cf04594aeee63208edda4b89553c
AmaChina <nanaama91@hotmail.com>
2023-05-16 21:51:08 -0700

    malloc hundred

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates arguements
+ * @ac: integer input
+ * @av: character input
+ * Return: pointer to new string otherwise NULL
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int p, q, r = 0, s = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (p = 0; p < ac; p++)
+	{
+		for (q = 0; av[p][q]; q++)
+			s++;
+	}
+
+	s += ac;
+
+	str = malloc(sizeof(char) * s + 1);
+
+	if (str == NULL)
+		return (NULL);
+	for (p = 0; p < ac; p++)
+	{
+		for (q = 0; av[p][q]; q++)
+		{
+			str[r] = av[p][q];
+			r++;
+		}
+		if (str[r] == '\0')
+		{
+			str[r++] = '\n';
+		}
+	}
+	return (str);
+}
