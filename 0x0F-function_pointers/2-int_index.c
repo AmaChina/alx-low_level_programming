a32fe8ad1bced2bf90c73050b02b75acf6760cad
AmaChina <nanaama91@hotmail.com>
2023-05-24 07:44:38 -0700

    index two

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * int_index -  searches for integer
+ * @array: array to work on
+ * @size: elements size
+ * @cmp: pointer to funtions
+ * Return: 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int n;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (n = 0; n < size; n++)
+	{
+		if (cmp(array[n]))
+			return (n);
+	}
+	return (-1);
+}
