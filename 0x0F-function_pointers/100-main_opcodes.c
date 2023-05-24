192ae959ce7caeeeb0ad3110ba14d1511981cc6c
AmaChina <nanaama91@hotmail.com>
2023-05-24 08:56:34 -0700

    opcodes hundred

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,31 +11,4 @@ int main(int argc, char *argv[])
 {
 	int b, n;
 	char *a;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-
-	b = atoi(argv[1]);
-
-	if (b < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-
-	a = (char *)main;
-
-	for (n = 0; n < bytes; n++)
-	{
-		if (n == b - 1)
-		{
-			printf("%02hhx\n", a[n]);
-			break;
-		}
-		printf("%02hhx ", a[n]);
-	}
-	return (0);
 }
