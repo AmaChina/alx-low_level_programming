77945262e040f5c538ed38f6c4ac0506b01952c9
AmaChina <nanaama91@hotmail.com>
2023-06-02 01:48:33 -0700

    still

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -27,27 +27,18 @@ int _strlen(const char *s)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *xin;
-	unsigned int len = 0;
 
 	xin = malloc(sizeof(list_t));
 
 	if (xin == NULL)
 	{
-		free(xin);
 		return (NULL);
 	}
 	xin->str = strdup(str);
 
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-	xin->len = len;
-	if (*head == NULL)
-		xin->next = NULL;
-	if (*head != NULL)
-		xin->next = *head;
-
+	xin->len = _strlen(str);
+	xin->next = *head;
 	xin = *head;
+
 	return (*head);
 }
