1b13ee4b007113a14453066ece64a455151f1651
AmaChina <nanaama91@hotmail.com>
2023-06-01 21:42:03 -0700

    threes

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * *add_node_end - adds nope to end of list
+ * @str: string
+ * @head: head of list
+ * Return: address of new element or NULL if fail
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *a, *b;
+	size_t c;
+
+	new_node = malloc(sizeof(list_t));
+	if (a == NULL)
+		return (NULL);
+
+	a->str = strdup(str);
+
+	for (c = 0; str[c]; c++)
+		;
+	a->len = c;
+	a->aft = NULL;
+	b = *head;
+
+	if (b == NULL)
+	{
+		*head = a;
+	}
+	else
+	{
+		while (b->aft != NULL)
+			b = b->aft;
+		b->aft = a;
+	}
+
+	return (*head);
+}
