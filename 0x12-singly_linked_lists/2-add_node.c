c68b395e5dc57f7a66a348fcab717e9900e0ef64
AmaChina <nanaama91@hotmail.com>
2023-06-02 01:42:44 -0700

    two

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 /**
+* _strlen - prints string length
+* @s: input value
+* Return: string length
+*/
+int _strlen(const char *s)
+{
+	int p = 0;
+
+	while (s[p] != '\0')
+	{
+		p++;
+	}
+	return (p);
+}
+
+/**
  * *add_node -  adds new node to beginning of list
  * @str: string to be added
  * @head: first node of list
