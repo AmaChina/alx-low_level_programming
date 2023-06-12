85815acc812c16d02f31d8211c5dbe4af02c1806
AmaChina <nanaama91@hotmail.com>
2023-06-12 14:51:10 -0700

    el two

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -40,3 +40,52 @@ void close_file(int fd)
                 exit(100);
         }
 }
+
+/**
+ * main - copies content of file to another
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int rd, wr, to, from;
+	char *buff;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	rd = read(from, buff, 1024);
+	buff = create_buffer(argv[2]);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(buff);
+			exit(98);
+		}
+
+		wr = write(to, buff, rd);
+		if (to == -1 || wr == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buff);
+			exit(99);
+		}
+
+		rd = read(from, buff, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+	}
+	while (rd > 0);
+
+	free(buff);
+	close_file(from);
+	close_file(to);
+
+	return (0);
+}
